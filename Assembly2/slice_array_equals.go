c1d99c24da980865dddf42d3e9a597f1eb6f815c
heeheemcgee <67600753+heeheemcgee@users.noreply.github.com>
2020-08-20 00:42:32 -0400

    deleting stuff

--- Assembly2/slice_array_equals.go
+++
@@ -1,7 +0,0 @@
-package main
-
-//StringSliceEquals takes two slice strings and returns true if they have
-//the same elements and false otherwise.
-func StringSliceEquals(patterns1, patterns2 []string) bool {
-	return true
-}
